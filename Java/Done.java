8dd86ac5f9f9653dcb82328f5e391379b2eb3e35
Milan Choudhary <choudharymilan08@gmail.com>
2025-09-06 21:10:21 +0530

    more questions done

--- Java/Done.java
+++ Java/Done.java
@@ -1,11 +1,23 @@
 public class Done {
     public static void main(String[] args) {
         //Equal Roots of BST 2236
+        //BST
         //Level Order Traversal in a binary tree
         //Check for children sum property
         //Insert in a BST
         //Merge Two BST
         //Deletion of a node in a BST
+        //Kth Smallest in a BST
+        //Two Sum in BST
+
+
+
+        //Heap
+
+
+
+
+        //Graph
         
         
         
