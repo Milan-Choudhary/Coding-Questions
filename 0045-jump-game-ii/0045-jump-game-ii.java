4c8c9649e859f31225a7d1e36f15101e54c8561c
Milan-Choudhary <165406595+Milan-Choudhary@users.noreply.github.com>
2026-09-11 10:57:47 +0530

    Time: 1 ms (99.79%), Space: 46.7 MB (99.1%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,42 +1,42 @@
 class Solution {
     public int jump(int[] nums) {
-
         if(nums.length == 1){
             return 0;
         }
+        
+        if(nums[0] >= nums.length - 1){
+            return 1;
+        }
+        
+        int reach = 0;
+        int maxnum = -1;
 
-        int max_ele = 0;
         int i = 0;
-        int index = 0;
-        int jumps = 0;
+        int c = 0;
 
         while(i < nums.length){
-            if(i + nums[i] >= nums.length - 1){
-                jumps += 1;
-                break;
-            }
 
-            max_ele = 0;
-            int j = i + 1;
-            while(j < nums.length && j<=nums[i] + i){
-                if(nums[j] + j >= max_ele){
-                    max_ele = nums[j] + j;
-                    index = j;
+           int j = i + nums[i];
+           c += 1;
+           int k = 0;
+
+           for(k = i + 1; k<=j; k++){
+                if(reach < k + nums[k]){
+                    reach = Math.max(reach,k + nums[k]);
+                    maxnum = k;
                 }
-                j += 1;
-            }
+                
+                if(reach >= nums.length - 1){
+                    return c + 1;
+                }
+           }
 
-            i = index;
-            jumps += 1;
-           
+           i = maxnum;
 
-            if(index >= nums.length - 1){
-                break;
-            }
+        }       
 
-        }
+        return 0;
 
-        return jumps;
 
-    }     
+    }
 }
